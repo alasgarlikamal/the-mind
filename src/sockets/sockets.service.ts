581542d80e60f4e1314a643cba8b0a97f8c80d4e
Kamal Alasgarli <elesgerlikamal04@gmail.com>
2022-12-04 12:46:17 +0400

    Added sockets.service.ts

--- src/sockets/sockets.service.ts
+++ src/sockets/sockets.service.ts
@@ -1,26 +1,57 @@
-import { Injectable } from '@nestjs/common';
-import { CreateSocketDto } from './dto/create-socket.dto';
-import { UpdateSocketDto } from './dto/update-socket.dto';
+import { InjectRedis } from '@liaoliaots/nestjs-redis';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Redis } from 'ioredis';
+import { Socket } from 'socket.io';
+import { v4 } from 'uuid';
+import { RoomDto } from './dto/room.dto';
+import { Room } from './types';
 
 @Injectable()
 export class SocketsService {
-  create(createSocketDto: CreateSocketDto) {
-    return 'This action adds a new socket';
+
+  constructor(@InjectRedis() private redis: Redis){}
+  
+  async createRoom(socket: Socket) {
+    const room : Room = {
+      id: v4(),
+      admin: socket.id,
+      joinedPlayers: [socket.id],
+      readyPlayers: []
+    }
+
+    await this.setRoom(room);
   }
 
-  findAll() {
-    return `This action returns all sockets`;
+  async joinRoom(socket: Socket, roomDto: RoomDto) {
+    const room = await this.getRoom(roomDto.roomId);
+
+    room.joinedPlayers.push(socket.id);
+
+    await this.setRoom(room);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} socket`;
+  async playerReady(socket: Socket, roomDto: RoomDto) {
+    const room = await this.getRoom(roomDto.roomId);
+
+    room.readyPlayers.push(socket.id);
+
+    await this.setRoom(room);
   }
 
-  update(id: number, updateSocketDto: UpdateSocketDto) {
-    return `This action updates a #${id} socket`;
+  async getRoom (roomId: string): Promise<Room> {
+    const room = await this.redis.get(`room:${roomId}`);
+
+    if (!room) throw new NotFoundException('Room not found');
+
+    return JSON.parse(room);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} socket`;
+  async setRoom (room: Room) {
+    try {
+      await this.redis.setex(`room:${room.id}`, 600, JSON.stringify(room));
+      
+    } catch (error) {
+      throw new Error('Error saving the room');
+    }
   }
 }
