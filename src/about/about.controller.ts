96c5c7a877abe0eb9dc32b8670c3f5dcedddb1ae
MurAd <muradisayevazer@gmail.com>
2022-12-09 17:47:24 +0400

    Added seed endpoint

--- src/about/about.controller.ts
+++ src/about/about.controller.ts
@@ -1,72 +1,116 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiBody, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AboutService } from './about.service';
 import { CreateAboutDto } from './dto/create-about.dto';
 import { UpdateAboutDto } from './dto/update-about.dto';
 import { About } from './entities/about.entity';
+import bootstrap from 'src/seeders/seed';
 
 @ApiTags('About')
 @Controller('about')
 export class AboutController {
-    constructor(private readonly aboutService: AboutService) { }
+  constructor(private readonly aboutService: AboutService) {}
 
-    @Get()
-    @ApiOkResponse({
-        status: 201,
-        description: 'Successfully returned all developers.'
-    })
-    @ApiNotFoundResponse({
-        status: 404,
-        description: 'Developers were not found'
-    })
-    findAll() {
-        return this.aboutService.findAll();
-    }
+  @Get()
+  @ApiOkResponse({
+    status: 201,
+    description: 'Successfully returned all developers.',
+  })
+  @ApiNotFoundResponse({
+    status: 404,
+    description: 'Developers were not found',
+  })
+  findAll() {
+    return this.aboutService.findAll();
+  }
 
-    @Post()
-    @ApiOkResponse({
-        status: 201,
-        description: 'The record has been successfully created.'
-    })
-    @ApiCreatedResponse({
-        description: 'Created record',
-        type: About,
-    })
-    @ApiBadRequestResponse({
-        status: 404,
-        description: 'Failed creating a new record.'
-    })
-    @ApiBody({ type: [CreateAboutDto] })
-    create(@Body() createAboutDto: CreateAboutDto) {
-        return this.aboutService.create(createAboutDto);
-    }
+  @Post()
+  @ApiOkResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
+  @ApiCreatedResponse({
+    description: 'Created record',
+    type: About,
+  })
+  @ApiBadRequestResponse({
+    status: 404,
+    description: 'Failed creating a new record.',
+  })
+  @ApiBody({ type: [CreateAboutDto] })
+  create(@Body() createAboutDto: CreateAboutDto) {
+    return this.aboutService.create(createAboutDto);
+  }
 
-    @Patch(':id')
-    @ApiOkResponse({
-        status: 201,
-        description: 'The record has been successfully updated.'
-    })
-    @ApiBadRequestResponse({
-        status: 404,
-        description: 'Failed updating the record.'
-    })
-    @ApiBody({ type: [UpdateAboutDto] })
-    @ApiParam({ name: 'id' })
-    update(@Param('id', ParseIntPipe) id: number, @Body() updateAboutDto: UpdateAboutDto) {
-        return this.aboutService.update(id, updateAboutDto);
-    }
+  @Patch(':id')
+  @ApiOkResponse({
+    status: 201,
+    description: 'The record has been successfully updated.',
+  })
+  @ApiBadRequestResponse({
+    status: 404,
+    description: 'Failed updating the record.',
+  })
+  @ApiBody({ type: [UpdateAboutDto] })
+  @ApiParam({ name: 'id' })
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateAboutDto: UpdateAboutDto,
+  ) {
+    return this.aboutService.update(id, updateAboutDto);
+  }
 
-    @Delete(':id')
-    @ApiOkResponse({
-        status: 201,
-        description: 'The record has been successfully deleted.'
-    })
-    @ApiBadRequestResponse({
-        status: 404,
-        description: 'Failed deleting the record.'
-    })
-    @ApiParam({ name: 'id' })
-    remove(@Param('id', ParseIntPipe) id: number) {
-        return this.aboutService.remove(id);
+  @Delete(':id')
+  @ApiOkResponse({
+    status: 201,
+    description: 'The record has been successfully deleted.',
+  })
+  @ApiBadRequestResponse({
+    status: 404,
+    description: 'Failed deleting the record.',
+  })
+  @ApiParam({ name: 'id' })
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.aboutService.remove(id);
+  }
+
+  @Get('seed')
+  @ApiOkResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
+  @ApiCreatedResponse({
+    description: 'Created record',
+    type: About,
+  })
+  @ApiBadRequestResponse({
+    status: 404,
+    description: 'Failed creating a new record.',
+  })
+  @ApiBody({ type: [CreateAboutDto] })
+  async seed() {
+    try {
+      await bootstrap();
+      return 'Seeding complete!';
+    } catch (error) {
+      throw error;
     }
+  }
 }
