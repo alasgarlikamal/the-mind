279c370a8441516a5ee07f93c27ccfdfe687961d
Kamal Alasgarli <elnuralasgarli@Kamals-MacBook-Pro.local>
2022-12-09 16:06:34 +0400

    removed db.entites.ts

--- src/config/db.entities.ts
+++
@@ -1,7 +0,0 @@
-import { Avatar } from 'src/avatars/entities/avatar.entity';
-import { User } from 'src/users/entities/user.entity';
-import { About } from 'src/about/entities/about.entity';
-import { ConfirmMailToken } from 'src/auth/entities/confirmMailToken.entity';
-import { ConfirmPasswordToken } from 'src/auth/entities/confirmPasswordToken.entity';
-
-export const entities = [User, Avatar, About, ConfirmMailToken, ConfirmPasswordToken];
