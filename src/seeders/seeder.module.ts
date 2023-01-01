c6d0d017901808f4eb5c81582a42b0a4ba55c48c
Kamal <elesgerlikamal04@gmail.com>
2023-01-01 16:37:48 +0400

    Added avatar seeding

--- src/seeders/seeder.module.ts
+++ src/seeders/seeder.module.ts
@@ -4,6 +4,9 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { config } from '../config/config';
 import { DatabaseConfig } from "src/config/database.config";
 import { SeederService } from "./seeder.service";
+import { AvatarsModule } from "src/avatars/avatars.module";
+import { UsersModule } from "src/users/users.module";
+import { AboutModule } from "src/about/about.module";
 
 @Module({
     imports: [ConfigModule.forRoot({
@@ -14,7 +17,8 @@ import { SeederService } from "./seeder.service";
     TypeOrmModule.forRootAsync({
         imports: [ConfigModule],
         useClass: DatabaseConfig
-    })],
+    }),
+    AvatarsModule, UsersModule, AboutModule],
     providers: [SeederService]
 })
 export class SeederModule {}
--- src/seeders/seeder.service.ts
+++ src/seeders/seeder.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { About } from 'src/about/entities/about.entity';
+import { AvatarsService } from 'src/avatars/avatars.service';
+import { Avatar } from 'src/avatars/entities/avatar.entity';
 import { DataSource, QueryRunner } from 'typeorm';
 
 @Injectable()
 export class SeederService {
 
-    constructor(private readonly dataSource: DataSource){}
+    constructor(private readonly dataSource: DataSource, private readonly avatarsService: AvatarsService){}
 
 
     async seed (){
@@ -50,6 +52,29 @@ export class SeederService {
                 role:"Frontend Developer", 
                 text:"ReactJs"
             }
+        ])
+        .execute();
+
+        await queryRunner.query('SET FOREIGN_KEY_CHECKS = 0;');
+        await queryRunner.query('TRUNCATE TABLE avatars;');
+        await queryRunner.query('SET FOREIGN_KEY_CHECKS = 1;');
+
+        await queryRunner.manager.createQueryBuilder()
+        .insert()
+        .into(Avatar)
+        .values([
+            { imageUrl: '/images/avatars/elsa.png' },
+            { imageUrl: '/images/avatars/french.png' },
+            { imageUrl: '/images/avatars/harry.png' },
+            { imageUrl: '/images/avatars/konichuwa.png' },
+            { imageUrl: '/images/avatars/kosa.png' },
+            { imageUrl: '/images/avatars/mexican.png' },
+            { imageUrl: '/images/avatars/midsommar.png' },
+            { imageUrl: '/images/avatars/prostoy.png' },
+            { imageUrl: '/images/avatars/shanshunshin.png' },
+            { imageUrl: '/images/avatars/walter.png' },
+            { imageUrl: '/images/avatars/woman.png' },
+            
         ])
         .execute();
 
