1aaddbb8df27481168b809c3a930653135385dab
Kamal <elesgerlikamal04@gmail.com>
2023-01-11 19:04:30 +0400

    added sockets controller

--- src/sockets/sockets.module.ts
+++ src/sockets/sockets.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { SocketsService } from './sockets.service';
 import { SocketsGateway } from './sockets.gateway';
 import { AuthModule } from 'src/auth/auth.module';
+import { SocketsController } from './sockets.controller';
 
 
 @Module({
   imports: [AuthModule],
+  controllers: [SocketsController],
   providers: [SocketsGateway, SocketsService]
 })
 export class SocketsModule {}
