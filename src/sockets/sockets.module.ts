3d18d1cc4eec87f02fa02088c6ee805ba6c87c42
Kamal <elesgerlikamal04@gmail.com>
2023-01-20 23:01:18 +0400

    imported usersmodule

--- src/sockets/sockets.module.ts
+++ src/sockets/sockets.module.ts
@@ -3,10 +3,11 @@ import { SocketsService } from './sockets.service';
 import { SocketsGateway } from './sockets.gateway';
 import { AuthModule } from 'src/auth/auth.module';
 import { SocketsController } from './sockets.controller';
+import { UsersModule } from 'src/users/users.module';
 
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
   controllers: [SocketsController],
   providers: [SocketsGateway, SocketsService]
 })
